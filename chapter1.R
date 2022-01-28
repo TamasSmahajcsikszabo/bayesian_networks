4552f95fb282e52a879d0dc67a95e77a04ee8f40
Tamas <41322635+TamasSmahajcsikszabo@users.noreply.github.com>
2022-01-28 23:04:08 +0100

    Update chapter1.R

--- chapter1.R
+++ chapter1.R
@@ -5,4 +5,11 @@ library(bnlearn)
 # arcs are interpreted as dependence rather than causal effects
 # i.e. child vertex depends on parent vertex
 
-DAG <- 
+DAG = empty.graph(nodes=c("A", "S", "E", "O", "R", "T"))
+DAG = set.arc(DAG, from = "A", to="E")
+DAG = set.arc(DAG, from = "S", to="E")
+DAG = set.arc(DAG, from = "E", to="O")
+DAG = set.arc(DAG, from = "E", to="R")
+DAG = set.arc(DAG, from = "R", to="T")
+DAG = set.arc(DAG, from = "O", to="T")
+plot(DAG)
