df11aaade70602deb03152f0363e6117a080a0ad
Tamas@fedora <tsmahajcsiks@avaya.com>
2024-06-16 23:20:40 +0200

    updated docs

--- chapter1.R
+++ chapter1.R
@@ -1,15 +1,25 @@
 library(bnlearn)
 
-# DAGs 
+# DAGs
 # no cyles are allowed
-# arcs are interpreted as dependence rather than causal effects
+# arcs are interpreted as dependence (direct dependence relationship) rather than causal effects
 # i.e. child vertex depends on parent vertex
+# paths: indirect relationship (indirect dependencies) - traversing the graph
+# cycles such as A -> ... -> A are not allowed
 
 DAG = empty.graph(nodes=c("A", "S", "E", "O", "R", "T"))
+
+# AGE direct influence on Education
 DAG = set.arc(DAG, from = "A", to="E")
+
+# GENDER direct influence on Education
 DAG = set.arc(DAG, from = "S", to="E")
+
+# Education's influences:
 DAG = set.arc(DAG, from = "E", to="O")
 DAG = set.arc(DAG, from = "E", to="R")
+
+# Residence and Occupation influence on Travel
 DAG = set.arc(DAG, from = "R", to="T")
 DAG = set.arc(DAG, from = "O", to="T")
 plot(DAG)
@@ -27,6 +37,9 @@ arc.set=matrix(c("A", "E", "S", "E", "E", "O", "E", "R", "R", "T", "O", "T"),
 arcs(DAG2)=arc.set
 plot(DAG2)
 
+# Global Distribution: the joint probability distribution of all the data (e.g. all possible pairs of values)
+# normally this is broken down to local distributions - one for each variables
+# assumtion is that if no arc is present, the variables are conditionally independent
 # to specify the joint prob. distribution over these variables
 # we add non-ordered states
 A.lv=c("young", "adult", "old")
@@ -44,14 +57,16 @@ T.lv=c("car", "train", "other")
 # this eq. shows how the dependencies encoded in the DAG map into 
 # the prob. space via the conditional dependencies
 # factorization is well defined, as there are no cycles
+# the set of all local d.s have fewer parameters than the glo. dist.
 # the eq. is a submodel/nested model of the global distr.
 
-# parentless variables are modelled as unidimensional prob. tables
+# parentless variables are modelled as unidimensional prob. tables, i.e. has no parent
 A.prob=array(c(0.30, 0.50, 0.20), dim=3, dimnames=list(A=A.lv))
 S.prob=array(c(0.60, 0.40), dim=2, dimnames=list(S=S.lv))
 
 # two dim. prob. tables
 # cols are levels of parent, distr. of variable is kept conditional on that level
+# ps of columns sum to 1
 O.prob=array(c(0.96, 0.04, 0.92, 0.08), dim=c(2,2), dimnames=list(O=O.lv, E=E.lv))
 R.prob=array(c(0.25, 0.75, 0.20, 0.80), dim=c(2,2), dimnames=list(R=R.lv, E=E.lv))
 
@@ -72,9 +87,10 @@ cpt=list(A=A.prob, S=S.prob, T=T.prob, O=O.prob, R=R.prob, E=E.prob)
 bn=custom.fit(DAG, cpt)
 nparams(bn)
 
+# Estimating the parameters
+
 # a more typical case: learning local distributions from the data
 survey=read.table("data/survey.txt", header=T, colClasses='factor')
-dag=bn.fit(survey)
 # conditional probabilities in the local distributions are estimated from the
 # empirical frequencies
 # this yields the classic frequentist maximum likelihood estimates
